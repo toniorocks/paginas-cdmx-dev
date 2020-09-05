2320bc2b54fa0e203ad4c986c13f2af9e25119ba
Marco Antonio Izaguirre Sierra <marco.izaguirre@live.com>
2020-09-05 12:22:34 -0500

    Directiva compartida

---
+++ src/app/shared/custom-validators.directive.ts
@@ -0,0 +1,32 @@
+import { FormGroup, AbstractControl, ValidatorFn } from '@angular/forms';
+
+// custom validator to check that two fields match
+export function MustMatch(controlName: string, matchingControlName: string) {
+    return (formGrgitoup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // return if another validator has already found an error on the matchingControl
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
+
+// /** A hero's name can't match the given regular expression */
+// export function MustMatch(controlname: string, confirmControlname: string): ValidatorFn | null {
+
+
+
+//     return null;
+
+
+// }
+
