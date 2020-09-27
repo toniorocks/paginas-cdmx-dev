e03207433bd61e90341e25f4f4f805166487aa04
=marco <=marco.izaguirre@live.com>
2020-09-27 01:03:19 -0500

    Falta el botón de envio y las pruebas con el back

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+declare var $: any;
+
 @Component({
 	selector: 'app-inicio',
 	templateUrl: './inicio.component.html',
@@ -12,43 +14,60 @@ export class InicioComponent implements OnInit {
 	recordatorio_a_: boolean = false;
 	descripcion: string = '';
 	registroForm: FormGroup;
+	verAviso_:boolean = false;
 
 	constructor(private _fb: FormBuilder) { }
 
 	ngOnInit(): void {
 		
 		this.registroForm = this._fb.group({
-			correo: ['', Validators.required, Validators.email],
-			numero: ['', Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
+			correo: ['', Validators.required],
+			numero: ['', Validators.required],
 			descripcion:['', Validators.maxLength(500)],
 			aviso:[false, Validators.required],
 			//terminos:[false, Validators.required]
 		});
-
-		// this.registroForm.statusChanges.subscribe(()=>{
-		// 	this.onValidate();
-		// });
+		document.getElementById('recordatorio-aviso').style.display = 'none';
+		this.registroForm.statusChanges.subscribe(()=>{
+			this.onValidate();
+		});
 
 	}
 
 	get f() { return this.registroForm.controls; }
 
-	descripcionChange():void{
-	}
-
 	onValidate(): void {
-		// this.f.value;
-		// console.log(this.f.correo);
-		// console.log(this.f.numero);
-		// if(this.f.correo.valid && this.f.numero.valid){
-		// 	console.log('val')
-		// }
+		if(this.f.correo.valid && this.f.numero.valid){
+			document.getElementById('recordatorio-aviso').style.display = 'initial';
+		}
+
+		if(this.f.aviso.value == true){
+			document.getElementById('recordatorio-aviso').style.display = 'none';
+			document.getElementById('alerta-recordatorio').style.display = 'none';
+		}
+
+		if(this.f.valid){
+
+			this.validForm = true;
+
+		}
+
 
 	}
 
-	
+	verAviso():void{
+		this.verAviso_ = true;
+		$("#avisoModal").modal('show');
+		document.getElementById('alerta-recordatorio').style.display = 'none';
+	}
 
-	onSubmit(){}
+	aceptarAviso(){
+		this.registroForm.controls.aviso.setValue(true);
+	}
+
+	onSubmit(){
+
+	}
 
 
 }
