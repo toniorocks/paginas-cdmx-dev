ae3636910a4933237c2bf85feb303cb85bc34f39
=marco <=marco.izaguirre@live.com>
2020-09-26 21:02:10 -0500

    Indicadores de campos del formulario

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+	selector: 'app-inicio',
+	templateUrl: './inicio.component.html',
+	styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+	validForm: boolean = false;
+	recordatorio_a_: boolean = false;
+	descripcion: string = '';
+	registroForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor(private _fb: FormBuilder) { }
+
+	ngOnInit(): void {
+		
+		this.registroForm = this._fb.group({
+			correo: ['', Validators.required, Validators.email],
+			numero: ['', Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
+			descripcion:['', Validators.maxLength(500)],
+			aviso:[false, Validators.required],
+			//terminos:[false, Validators.required]
+		});
+
+		// this.registroForm.statusChanges.subscribe(()=>{
+		// 	this.onValidate();
+		// });
+
+	}
+
+	get f() { return this.registroForm.controls; }
+
+	descripcionChange():void{
+	}
+
+	onValidate(): void {
+		// this.f.value;
+		// console.log(this.f.correo);
+		// console.log(this.f.numero);
+		// if(this.f.correo.valid && this.f.numero.valid){
+		// 	console.log('val')
+		// }
+
+	}
+
+	
+
+	onSubmit(){}
+
 
 }
--- src/app/shared/custom-validators.directive.ts
+++ src/app/shared/custom-validators.directive.ts
@@ -10,3 +10,5 @@ export const passwordMatch: ValidatorFn = (control: FormGroup): ValidationErrors
 
 
 
+
+
